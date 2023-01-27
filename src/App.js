601bdd540cd32c5de7ac561710bb7d229cc415c8
shamim ahmed <sahmed@ibrainnyc.org>
2023-01-27 17:45:33 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ViewTransferTable from './viewtransfertable/ViewTransferTable';
-//import ViewDataTransfer from './ViewDataTransfer';
-//import ViewTransferCopy from './studentform/viewformCopy/ViewTransferCopy';
-//import Form from './studentform/transferform/Form';
-//import SearchBar from './search/SearchBar';
-//import FileUploadPage from './studentform/Upload';
-//import BookData from "./Data.json";
+import React, { useState } from "react";
+import { useForm, FormProvider, Controller, useFormContext } from "react-hook-form";
+// //import ViewTransferTable from './viewtransfertable/ViewTransferTable';
+//import Form from './components/forms/Form';
+// //import ViewDataTransfer from './ViewDataTransfer';
+// //import ViewTransferCopy from './studentform/viewformCopy/ViewTransferCopy';
+// //import Form from './studentform/transferform/Form';
+// //import SearchBar from './search/SearchBar';
+// //import FileUploadPage from './studentform/Upload';
+// //import BookData from "./Data.json";
+import { TextField } from '@mui/material'
+
+
+// function App() {
+//   return (
+//     <div>
+//       {/*<Form />
+//       <FileUploadPage />
+//       <Home />
+//       <SearchBar placeholder="Book Name..." 
+//         data={BookData} />
+//         <ViewTransferCopy />*/}
+//         {/* <ViewTransferTable /> */}
+
+//         <Form />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 
 function App() {
+
+  //let data;
+
+  // const [texting, setTexting] = useState('Room')
+
+  const { control, handleSubmit } = useForm();
+  //const methods = useForm();
+
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    console.log(data);
+}
+
+// const handleChange = (e) => {
+//   setTexting(e.target.value)
+// }
+
   return (
-    <div>
-      {/*<Form />
-      <FileUploadPage />
-      <Home />
-      <SearchBar placeholder="Book Name..." 
-        data={BookData} />
-        <ViewTransferCopy />*/}
-        <ViewTransferTable />
-    </div>
+    //   <FormProvider {...methods}>
+    //   <div className="container py-4">
+    //     {/* <Form methods={methods} /> */}
+    //     <Form methods={methods} />
+    //   </div>
+    // </FormProvider>
+
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller 
+          name='location' 
+          control={control} 
+          defaultValue='' 
+          render={({ field: { onChange, value }}) => (
+              <TextField 
+                  value={value}  
+                  sx={{width: '22ch'}} 
+                  onChange={onChange}>
+              </TextField>
+          )} />
+
+      <button type='submit'>Submit</button>
+    </form>
+    </>
+    
   );
 }
 
-export default App;
+export default App
--- src/viewtransfertable/ViewTransferTable.jsx
+++ src/viewtransfertable/ViewTransferTable.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useState, useRef } from "react";
+import { Button } from '@mui/material';
 
 const Container = styled.div`
   display: flex;
@@ -8,12 +10,57 @@ const Container = styled.div`
 
 `
 
-const ViewTransferTable = () => {
+// const ViewTransferTable = () => {
+//   return (
+//     <Container>
+//       <h1>View Transfer Table</h1>
+
+     
+export default function FilePreviewer() {
+  const [imagePreview, setImagePreview] = useState(null);
+  const [videoPreview, setVideoPreview] = useState(null);
+
+  const filePicekerRef = useRef();
+  function previewFile(e) {
+    // Reading New File (open file Picker Box)
+    const reader = new FileReader();
+    // Gettting Selected File (user can select multiple but we are choosing only one)
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      reader.readAsDataURL(selectedFile);
+    }
+    // As the File loaded then set the stage as per the file type
+    reader.onload = (readerEvent) => {
+      if (selectedFile.type.includes("image")) {
+        setImagePreview(readerEvent.target.result);
+      } else if (selectedFile.type.includes("video")) {
+        setVideoPreview(readerEvent.target.result);
+      }
+    };
+  }
+
+  function clearFiles() {
+    setVideoPreview(null);
+  }
+  
   return (
     <Container>
-      <h1>View Transfer Table</h1>
+      <h1>Preview Image/Video</h1>
+      <div className="btn-container">
+        <input ref={filePicekerRef} accept="image/*, video/*" onChange={previewFile} type="file" hidden />
+        <Button className="btn" onClick={() => filePicekerRef.current.click()}>Choose Video File</Button>
+        <Button className="btn" onClick={clearFiles}>x</Button>
+      </div>
+      <div className="preview">
+        {imagePreview != null && <img src={imagePreview} alt="" />}
+        {videoPreview != null && <video controls src={videoPreview}></video>}
+      </div>
     </Container>
-  )
+  );
 }
 
-export default ViewTransferTable
+//     </Container>
+//   )
+// }
+
+// export default ViewTransferTable
