7598178ca1a2675efff86ec75805ddcc8811574e
shamim ahmed <sahmed@ibrainnyc.org>
2023-02-08 11:47:16 -0500

    date-filter

--- src/App.js
+++ src/App.js
@@ -1,65 +1,68 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
+import DateRangeFilter from './components/date-filter/DateRangeFilter';
 // //import ViewTransferTable from './viewtransfertable/ViewTransferTable';
 //import Form from './components/forms/Form';
 // //import ViewDataTransfer from './ViewDataTransfer';
 // //import ViewTransferCopy from './studentform/viewformCopy/ViewTransferCopy';
-// //import Form from './studentform/transferform/Form';
+import Form from './studentform/transferform/Form';
 // //import SearchBar from './search/SearchBar';
 // //import FileUploadPage from './studentform/Upload';
 // //import BookData from "./Data.json";
-import { TextField } from '@mui/material'
-
-
-// function App() {
-//   return (
-//     <div>
-//       {/*<Form />
-//       <FileUploadPage />
-//       <Home />
-//       <SearchBar placeholder="Book Name..." 
-//         data={BookData} />
-//         <ViewTransferCopy />*/}
-//         {/* <ViewTransferTable /> */}
-
-//         <Form />
-//     </div>
-//   );
-// }
-
-// export default App;
-
+//import { TextField } from '@mui/material'
 
 
 function App() {
-  const { control, handleSubmit } = useForm();
-
-  const onSubmit = (data, e) => {
-    e.preventDefault();
-    console.log(data);
-}
-
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller 
-          name='location' 
-          control={control} 
-          defaultValue='' 
-          render={({ field: { onChange, value }}) => (
-              <TextField 
-                  value={value}  
-                  sx={{width: '22ch'}} 
-                  onChange={onChange}>
-              </TextField>
-          )} />
+    <div>
+      {/*<Form />
+      <FileUploadPage />
+      <Home />
+      <SearchBar placeholder="Book Name..." 
+        data={BookData} />
+        <ViewTransferCopy />*/}
+        {/* <ViewTransferTable />
+        <Form /> */}
 
-        <button type='submit'>Submit</button>
-      </form>
-    </>
-    
+        <DateRangeFilter />
+
+    </div>
   );
 }
 
-export default App
+export default App;
+
+
+
+// function App() {
+//   const { control, handleSubmit } = useForm();
+
+//   const onSubmit = (data, e) => {
+//     e.preventDefault();
+//     console.log(data);
+// }
+
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit(onSubmit)}>
+//         <Controller 
+//           name='location' 
+//           control={control} 
+//           defaultValue='' 
+//           render={({ field: { onChange, value }}) => (
+//               <TextField 
+//                   value={value}  
+//                   sx={{width: '22ch'}} 
+//                   onChange={onChange}>
+//               </TextField>
+//           )} />
+
+//         <button type='submit'>Submit</button>
+//       </form>
+//     </>
+    
+//   );
+// }
+
+// export default App
---
+++ src/components/date-filter/dateFilterData.js
@@ -0,0 +1,57 @@
+export const Users = [
+    {
+        id: 1,
+        provider: 'Gabriela Zaheer',
+        student: 'Esther Ravitz',
+        typeOf: 'academics_type',
+        serviceType: 'Academics',
+        sessionDate: '01/17/2023',
+        sessionEnd: '01/17/2023',
+        submitDate: '01/17/2023',
+        hoursSubmit: '5'
+    },
+    {
+        id: 2,
+        provider: 'Rachel Winigrad',
+        student: 'Yaffa Nisanova',
+        typeOf: 'occupational_therapy',
+        serviceType: 'Occupational Therapy',
+        sessionDate: '01/19/2023',
+        sessionEnd: '01/19/2023',
+        submitDate: '01/19/2023',
+        hoursSubmit: '0'
+    },
+    {
+        id: 3,
+        provider: 'Diquan Williams',
+        student: 'Amari James Chaperon',
+        typeOf: 'music_therapy',
+        serviceType: 'Music Therapy',
+        sessionDate: '01/17/2023',
+        sessionEnd: '01/17/2023',
+        submitDate: '01/17/2023',
+        hoursSubmit: '1'
+    },
+    {
+        id: 4,
+        provider: 'Melissa Wasserman',
+        student: 'Raricha(Ellie) Lummayos',
+        typeOf: 'academics_type',
+        serviceType: 'Academics',
+        sessionDate: '01/17/2023',
+        sessionEnd: '01/17/2023',
+        submitDate: '01/17/2023',
+        hoursSubmit: '3'
+    },
+    {
+        id: 5,
+        provider: 'Arianna Robbins',
+        student: 'Edyan Rodriguez',
+        typeOf: 'music_therapy',
+        serviceType: 'Music Therapy',
+        sessionDate: '01/20/2023',
+        sessionEnd: '01/20/2023',
+        submitDate: '01/20/2023',
+        hoursSubmit: '1'
+    }
+]
