a752391181e65e87f8dee892af05ba98059108e6
shamim ahmed <sahmed@ibrainnyc.org>
2023-06-02 15:20:15 -0400

    loop-table

--- src/components/date-filter/DateFilterTypes.jsx
+++ src/components/date-filter/DateFilterTypes.jsx
@@ -57,3 +57,36 @@ const DateFilterTypes = () => {
 }
 
 export default DateFilterTypes
+
+// if(result && Array.isArray(result)) {
+        //     const data = result;
+
+        // const studentTotals = {};
+
+        // data.forEach((session) => {
+        //     const studentID = session.student_id;
+
+        //     if(!studentTotals[studentID]) {
+        //         studentTotals[studentID] = {
+        //             num_th_sessions: session.num_th_sessions,
+        //             percentage_th: session.percentage_th,
+        //             total_sessions: session.total_sessions,
+        //             total_th_hours: session.total_th_hours,
+        //         };
+        //     } 
+        //     else {
+        //         studentTotals[studentID].num_th_sessions += session.num_th_sessions;
+        //     }
+        // });
+
+        // for (const studentID in studentTotals) {
+        //     if (studentTotals.hasOwnProperty(studentID)) {
+        //       console.log("Student ID:", studentID);
+        //       console.log("Total TH Sessions:", studentTotals[studentID].num_th_sessions);
+        //       console.log("Percentage TH:", studentTotals[studentID].percentage_th);
+        //       console.log("Total Sessions:", studentTotals[studentID].total_sessions);
+        //       console.log("Total TH Hours:", studentTotals[studentID].total_th_hours);
+        //       console.log("\n");
+        //     }
+        //   }
+        // }
